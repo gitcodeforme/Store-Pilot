393ee8041d329250a728a59372bec35874f42ac9
gitcodeforme <gitcodeforme@gmail.com>
2026-02-19 15:00:57 +0530

    Fix customer dto

--- store-management/src/main/java/com/store/management/model/Customer.java
+++ store-management/src/main/java/com/store/management/model/Customer.java
@@ -7,24 +7,27 @@ import lombok.*;
 @Entity
 @Getter
 @Setter
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table(name = "customer")
 public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
     private BigInteger customerId;
 
     @Column(name = "customer_code", unique = true)
     private String customerCode;
 
+    @Column(name = "customer_name")   // ✅ ADD THIS
     private String customerName;
 
+    @Column(name = "mobile_number")   // ✅ ADD THIS
     private String mobileNumber;
 
     @Lob
+    @Column(name = "address")         // optional but recommended
     private String address;
 }
-
